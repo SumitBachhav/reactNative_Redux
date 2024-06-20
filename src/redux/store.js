c9d7fd248b23871db4127d0e914c48d580c05001
SumitBachhav <sumitb2156@gmail.com>
2024-06-20 11:13:01 +0530

    redux with persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,26 @@
-import {configureStore} from "@reduxjs/toolkit"
+import {applyMiddleware, combineReducers, configureStore} from "@reduxjs/toolkit"
 import CounterSlice from "./features/CounterSlice"
+import { persistReducer, persistStore } from "redux-persist";
+import AsyncStorage from "@react-native-async-storage/async-storage";
  
- export const store = configureStore({
-	reducer: {
-        counter : CounterSlice
-    },
-	})
+const reducers = combineReducers({
+    counter: CounterSlice,
+});
+
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+    whitelist: ['counter']
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware => getDefaultMiddleware({serializableCheck: false})
+});
+
+const persistor = persistStore(store);
+
+export {store, persistor};
